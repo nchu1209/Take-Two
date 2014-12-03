78e223b8e722f6768c5246c3cc1e65557bb745d6
amyenrione <amyenrione@utexas.edu>
2014-12-03 13:23:06 -0600

    fixed response redirect bug

--- CustomerTransactionSearch.aspx.vb
+++ CustomerTransactionSearch.aspx.vb
@@ -7,14 +7,21 @@
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         If IsPostBack = False Then
-            Dim strAccountNumber As String
-            strAccountNumber = "100000" & Request.QueryString("ID").Substring(6, 4)
-            Session("AccountNumber") = strAccountNumber
+            If Request.QueryString("ID") Is Nothing Then
+                DBTransactions.GetAllTransactions(Session("AccountNumber"))
+                DBAccount.GetAccountNameByAccountNumber(Session("AccountNumber"))
+                lblAccountName.Text = DBAccount.AccountsDataset5.Tables("tblAccounts").Rows(0).Item("AccountName")
+                Search()
+            Else
+                Dim strAccountNumber As String
+                strAccountNumber = "100000" & Request.QueryString("ID").Substring(6, 4)
+                Session("AccountNumber") = strAccountNumber
 
-            DBTransactions.GetAllTransactions(Session("AccountNumber"))
-            DBAccount.GetAccountNameByAccountNumber(Session("AccountNumber"))
-            lblAccountName.Text = DBAccount.AccountsDataset5.Tables("tblAccounts").Rows(0).Item("AccountName")
-            Search()
+                DBTransactions.GetAllTransactions(Session("AccountNumber"))
+                DBAccount.GetAccountNameByAccountNumber(Session("AccountNumber"))
+                lblAccountName.Text = DBAccount.AccountsDataset5.Tables("tblAccounts").Rows(0).Item("AccountName")
+                Search()
+            End If  
         End If
         DBTransactions.GetAllTransactions(Session("AccountNumber").ToString)
     End Sub
