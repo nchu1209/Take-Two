3b1e48407ae26f7b44991b7e95b2d7b806282cda
caking789 <caking789@gmail.com>
2014-12-05 08:14:04 -0600

    updates

--- CustomerPortfolioDetail.aspx.designer.vb
+++ CustomerPortfolioDetail.aspx.designer.vb
@@ -21,4 +21,94 @@ Partial Public Class CustomerPortfolioDetail
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents lblPortfolioStatus As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''lblBalanced control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents lblBalanced As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''lblTotalValue control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents lblTotalValue As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''PanelCash control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents PanelCash As Global.System.Web.UI.WebControls.Panel
+
+    '''<summary>
+    '''gvCash control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents gvCash As Global.System.Web.UI.WebControls.GridView
+
+    '''<summary>
+    '''SqlDataSource2 control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents SqlDataSource2 As Global.System.Web.UI.WebControls.SqlDataSource
+
+    '''<summary>
+    '''PanelNoCash control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents PanelNoCash As Global.System.Web.UI.WebControls.Panel
+
+    '''<summary>
+    '''PanelStockPortion control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents PanelStockPortion As Global.System.Web.UI.WebControls.Panel
+
+    '''<summary>
+    '''gvStockPortion control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents gvStockPortion As Global.System.Web.UI.WebControls.GridView
+
+    '''<summary>
+    '''SqlDataSource1 control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents SqlDataSource1 As Global.System.Web.UI.WebControls.SqlDataSource
+
+    '''<summary>
+    '''PanelNoStocks control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents PanelNoStocks As Global.System.Web.UI.WebControls.Panel
 End Class
--- CustomerPortfolioDetail.aspx.vb
+++ CustomerPortfolioDetail.aspx.vb
@@ -1,18 +1,87 @@
-﻿Public Class CustomerPortfolioDetail
+﻿Option Strict On
+
+Public Class CustomerPortfolioDetail
     Inherits System.Web.UI.Page
     Dim DBAccounts As New ClassDBAccounts
+    Dim mdecTotalStockValue As Decimal
+    Dim mdecTotalPortfolioValue As Decimal
+    Dim mdecTotalCashValue As Decimal
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim i As Integer
+        Dim intOrdinary As Integer
+        Dim intIndex As Integer
+        Dim intMutual As Integer
+        'Dim intETF As Integer
 
-        If Session("CustomerFirstName") Is Nothing Then
-            Response.Redirect("CustomerLogin.aspx")
+
+        If IsPostBack = False Then
+            If Session("CustomerFirstName") Is Nothing Then
+                Response.Redirect("CustomerLogin.aspx")
+            End If
+
+            DBAccounts.GetAccountByCustomerNumber(Session("CustomerNumber").ToString)
+
+            If DBAccounts.AccountsDataset4.Tables("tblAccounts").Rows.Count = 0 Then
+                Response.Redirect("CustomerCreateBankAccount.aspx")
+            End If
         End If
 
-        DBAccounts.GetAccountByCustomerNumber(Session("CustomerNumber").ToString)
+        If gvStockPortion.Rows.Count = 0 Then
+            mdecTotalStockValue = 0
+            PanelStockPortion.Visible = False
+            PanelNoStocks.Visible = True
+        Else
 
-        If DBAccounts.AccountsDataset4.Tables("tblAccounts").Rows.Count = 0 Then
-            Response.Redirect("CustomerCreateBankAccount.aspx")
+
+            For i = 0 To gvStockPortion.Rows.Count - 1
+                mdecTotalStockValue += CDec(gvStockPortion.Rows(i).Cells(4).Text)
+
+                If gvStockPortion.Rows(i).Cells(3).Text = "Ordinary" Or gvStockPortion.Rows(i).Cells(3).Text = "Ordinary Stocks" Then
+                    intOrdinary += 1
+                End If
+
+                If gvStockPortion.Rows(i).Cells(3).Text = "Index Fund" Then
+                    intIndex += 1
+                End If
+
+                If gvStockPortion.Rows(i).Cells(3).Text = "Mutual Fund" Then
+                    intMutual += 1
+                End If
+
+            Next
         End If
+
+        If gvCash.Rows(0).Cells(0).Text Is DBNull.Value Then
+            lblBalanced.Text = "nada"
+            Exit Sub
+        End If
+        mdecTotalCashValue = CDec(gvCash.Rows(0).Cells(0).Text)
+        lblBalanced.Text = mdecTotalCashValue.ToString
+        Exit Sub
+
+        If mdecTotalCashValue = 0 Then
+            PanelCash.Visible = False
+            PanelNoCash.Visible = True
+            mdecTotalCashValue = 0
+
+
+            mdecTotalPortfolioValue = mdecTotalStockValue + mdecTotalCashValue
+
+
+            lblTotalValue.Text = mdecTotalPortfolioValue.ToString("C2")
+
+            If intIndex >= 1 And intOrdinary >= 2 And intMutual >= 1 Then
+                Session("Balanced") = True
+                lblBalanced.Text = "Your account is balanced."
+            Else
+                Session("Balanced") = False
+                lblBalanced.Text = "Your account is not balanced."
+            End If
+
+        End If
+
+
     End Sub
 
 End Class
